d5626767a24837905b891cc618ab9ae5cad4d7d8
AndrewSmith <a.smith_662@yahoo.com>
2022-01-09 09:06:10 -0500

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { ContactComponent } from './components/contact/contact.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'contact', component: ContactComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PhotosComponent,
+    ContactComponent,
   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
