acc124955f758a63a6753f111ef1a6c2aec9ecaf
AndrewSmith <a.smith_662@yahoo.com>
2022-01-12 15:54:04 -0500

    update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -13,16 +13,16 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
     
-    if (this.window >= 601) this.mybreakpoint = true ? 20 : 1;
-    else if (this.window <= 600) this.mybreakpoint = true ? 16 : 1;
-    else if (this.window <= 400) this.mybreakpoint = true ? 2 : 1;
-    else if (this.window <= 200) this.mybreakpoint = true ? 8 : 1;
+    if (this.window >= 601) this.mybreakpoint = true ? 64 : 1;
+    else if (this.window <= 600) this.mybreakpoint = true ? 32 : 1;
+    else if (this.window <= 400) this.mybreakpoint = true ? 4 : 1;
+    else if (this.window <= 200) this.mybreakpoint = true ? 1 : 4;
   }
 
   handleSize(event) {
-    if (this.window >= 601) this.mybreakpoint = true ? 20 : 1;
-    else if (this.window <= 600) this.mybreakpoint = true ? 16 : 1;
-    else if (this.window <= 400) this.mybreakpoint = true ? 2 : 1;
-    else if (this.window <= 200) this.mybreakpoint = true ? 8 : 1;
+    if (this.window >= 601) this.mybreakpoint = true ? 64 : 1;
+    else if (this.window <= 600) this.mybreakpoint = true ? 32 : 1;
+    else if (this.window <= 400) this.mybreakpoint = true ? 4 : 1;
+    else if (this.window <= 200) this.mybreakpoint = true ? 1 : 1;
   }
 }
